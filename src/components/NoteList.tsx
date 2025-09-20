7929bd73b58f4e2d754008fea5fab5d42eb6b42f
Zarman-oss <zarmanimran@gmail.com>
2025-09-20 02:53:36 -0500

    .

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,26 +1,26 @@
-export default function NoteList({ notes }) {
+export default function NoteList({ notes }: { notes: [] }) {
   return (
     <div className='mt-6 grid-auto'>
       {notes.map((note) => (
         <div
           key={note.id}
-          className='relative bg-yellow-100 text-dark p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200 border border-yellow-200'
+          className='relative bg-yellow-100 text-dark p-4 rounded-lg  border border-yellow-200'
         >
           {/* Fake pin effect */}
           <span className='absolute -top-1 left-4 w-3 h-3 bg-danger rounded-full shadow-md'></span>
 
           <div className='flex items-center justify-between mb-2'>
-            <span className='text-sm-fluid font-semibold text-zinc-500'>
-              #{note.id}
-            </span>
+            <div className='text-sm-fluid rounded-full h-6 w-6 flex justify-center items-center bg-neutral font-semibold text-dark'>
+              {note.id}
+            </div>
             <span
               className={`px-2 py-0.5 text-sm-fluid rounded-full font-medium
               ${
-                note.priority === 'high'
-                  ? 'bg-red-500/20 text-red-700'
-                  : note.priority === 'medium'
-                  ? 'bg-yellow-500/20 text-yellow-700'
-                  : 'bg-green-500/20 text-green-700'
+                note.priority === 'High'
+                  ? 'bg-red-500 text-dark uppercase text-sm-fluid'
+                  : note.priority === 'Medium'
+                  ? 'bg-yellow-500 text-dark uppercase text-sm-fluid'
+                  : 'bg-green-500 text-dark uppercase text-sm-fluid'
               }`}
             >
               {note.priority}
